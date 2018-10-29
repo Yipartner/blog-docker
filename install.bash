3da4eb25ffa84eb9c420820c49155d6a089b86a0
root <root@localhost.localdomain>
2018-10-29 18:32:16 +0800

    env add

---
+++ install.bash
@@ -0,0 +1,7 @@
+#/bin/bash
+. .env
+cp ./www/pub2.0/.env.example ./www/pub2.0/.env
+docker exec dnmp_php-fpm_1 composer update -d /var/www/html/pub2.0
+docker exec dnmp_mysql_1 mysqladmin -u"$DB_USERNAME" -p"$DB_PASSWORD" create pub;
+docker exec dnmp_php-fpm_1 php /var/www/html/pub2.0/artisan k:g
+docker exec dnmp_php-fpm_1 php /var/www/html/pub2.0/artisan migrate
