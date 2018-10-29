01f4ec7cdde25c6edc3a9a143e66f69bc07a2a3a
小伙伴 <1350690718@qq.com>
2018-10-29 19:06:44 +0800

    脚本成功执行结束语

--- install.bash
+++ install.bash
@@ -6,3 +6,4 @@ docker exec dnmp_php-fpm_1 composer update -d /var/www/html/pub2.0
 docker exec dnmp_mysql_1 mysqladmin -u"$DB_USERNAME" -p"$DB_PASSWORD" create "$PUB_DATABASE";
 docker exec dnmp_php-fpm_1 php /var/www/html/pub2.0/artisan k:g
 docker exec dnmp_php-fpm_1 php /var/www/html/pub2.0/artisan migrate
+echo "SUCCESS INSTALL 0(^_^)0"
